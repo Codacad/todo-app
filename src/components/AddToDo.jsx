db64a4c989d8fe77ed5780cf92026e4476f21df3
Mohd Rizwan <iamrizwan40@gmail.com>
2024-03-31 14:01:51 +0300

    changed the button to icon and put on top

--- src/components/AddToDo.jsx
+++ src/components/AddToDo.jsx
@@ -1,7 +1,8 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import toast from "react-hot-toast";
+import { FaArrowUp } from "react-icons/fa";
 
-const AddToDo = ({state, dispatch}) => {
+const AddToDo = ({ state, dispatch }) => {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const addTask = (todo) => {
@@ -17,8 +18,27 @@ const AddToDo = ({state, dispatch}) => {
     setDescription("");
   };
   return (
-    <div className="addTodo sticky bottom-0 md:w-[50%] w-[100%] mx-auto mt-8 bg-glass p-2 shadow-lg rounded-md max-md:mb-4 flex flex-col gap-4">
-      <h1 className="text-center text-2xl font-bold mt-4 text-gray-500">Add Task</h1>
+    <div className="addTodo sticky bottom-0 md:w-[50%] w-[100%] mx-auto mt-8 bg-gray-200 p-2 shadow-2xl rounded-md max-md:mb-4 flex flex-col gap-4">
+      <div className="flex justify-between items-center">
+        <h1 className="text-2xl font-bold mt-4 text-gray-500">
+          Add Task
+        </h1>
+        <button
+          onClick={() =>
+            addTask({
+              id: state.length,
+              title,
+              description,
+              completed: false,
+              favorite: false,
+              createdAt: new Date().getTime(),
+            })
+          }
+          className="w-8 h-8 ml-auto bg-blue-500 px-2 py-2 focus:scale-95 active:scale-90 text-white rounded-full"
+        >
+          <FaArrowUp />
+        </button>
+      </div>
       <input
         className="focus:ring-2 rounded-md px-4 py-1 focus:ring-blue-500 outline-none border-[1px] border-gray-300"
         type="text"
@@ -34,21 +54,6 @@ const AddToDo = ({state, dispatch}) => {
         onChange={(e) => setDescription(e.target.value)}
         placeholder="Enter Description..."
       />
-      <button
-        onClick={() =>
-          addTask({
-            id: state.length,
-            title,
-            description,
-            completed: false,
-            favorite:false,
-            createdAt: new Date().getTime(),
-          })
-        }
-        className="bg-blue-500 px-2 py-2 focus:scale-95 active:scale-90 text-white rounded-md"
-      >
-        Add Task
-      </button>
     </div>
   );
 };
